90eec7379d69b3386253badafe08b471092d1ec3
Brent Sorrentino <brent@coronalabs.com>
2016-02-08 16:00:46 -1000

    Include UI support for incentivized/rewarded video ads.

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,4 +1,6 @@
 This sample project exhibits basic utilization of the AppLovin plugin. This plugin allows developers to monetize users through AppLovin ads.
 
+Note that to receive incentivized/rewarded video ads in your app, you must enable and configure the feature in the AppLovin developer portal.
+
 For detailed information on this plugin, see:
 http://docs.coronalabs.com/plugin/applovin/
--- main.lua
+++ main.lua
@@ -36,8 +36,21 @@ local sdkKey = "[YOUR-SDK-KEY]"
 
 -- Set local variables
 local setupComplete = false
+local useIncentivizedRewarded = false
+local alertIncentivizedRewarded = true
 local loadButton
 local showButton
+
+-- Create asset image sheet
+local assets = graphics.newImageSheet( "assets.png",
+	{
+		frames = {
+			{ x=0, y=0, width=35, height=35 },
+			{ x=0, y=35, width=35, height=35 },
+		},
+		sheetContentWidth=35, sheetContentHeight=70
+	}
+)
 
 -- Create object to visually prompt action
 local prompt = display.newPolygon( mainGroup, 62, 210, { 0,-12, 12,0, 0,12 } )
@@ -119,24 +132,73 @@ local function adListener( event )
 		print( "AppLovin event: " .. tostring(event.type) .. " ad closed/hidden" )
 		updateUI( { enable={ loadButton }, disable={ showButton }, promptTo=loadButton } )
 
+	-- The user clicked/tapped an ad
+	elseif ( event.phase == "clicked" ) then
+		print( "AppLovin event: " .. tostring(event.type) .. " ad clicked/tapped" )
+
 	-- The ad failed to load
 	elseif ( event.phase == "failed" ) then
 		print( "AppLovin event: " .. tostring(event.type) .. " ad failed to load" )
+
+	-- The user declined to view a rewarded/incentivized video ad
+	elseif ( event.phase == "declinedToView" ) then
+		print( "AppLovin event: user declined to view " .. tostring(event.type) .. " ad" )
+		updateUI( { enable={ loadButton }, disable={ showButton }, promptTo=loadButton } )
+
+	-- The user viewed a rewarded/incentivized video ad
+	elseif ( event.phase == "validationSucceeded" ) then
+		print( "AppLovin event: " .. tostring(event.type) .. " ad viewed and reward approved by AppLovin server" )
+		local alert = native.showAlert( "Note", "AppLovin reward of " .. tostring(event.data.amount) .. " " .. tostring(event.data.currency) .. " registered!", { "OK" } )
+		updateUI( { disable={ showButton } } )
+
+	-- The incentivized/rewarded video ad and/or reward exceeded quota, failed, or was rejected
+	elseif ( event.phase == "validationExceededQuota" or event.phase == "validationFailed" or event.phase == "validationRejected" ) then
+		print( "AppLovin event: " .. tostring(event.type) .. " ad and/or reward exceeded quota, failed, or was rejected" )
 	end
 end
 
 
 -- Button handler function
-local function onButtonRelease( event )
+local function uiEvent( event )
 
 	if ( event.target.id == "load" ) then
-		applovin.load()
+		applovin.load( useIncentivizedRewarded )
 	elseif ( event.target.id == "show" ) then
-		applovin.show()
+		applovin.show( useIncentivizedRewarded )
+	elseif ( event.target.id == "useIncentivizedRewarded" ) then
+		if ( event.target.isOn == true ) then
+			useIncentivizedRewarded = true
+			-- Initially alert for incentivized/rewarded setup
+			if ( alertIncentivizedRewarded == true ) then
+				alertIncentivizedRewarded = false
+				local alert = native.showAlert( "Note", 'To receive incentivized/rewarded video ads in your app, ensure that you have enabled the feature in the AppLovin developer portal. Also note that activation of this feature does not take effect instantaneously, so it may require some time before these ads can be served.', { "OK" } )
+			end
+		else
+			useIncentivizedRewarded = false
+		end
 	end
 	return true
 end
 
+
+-- Create rewarded/incentivized switch/label
+local irSwitch = widget.newSwitch(
+	{
+		sheet = assets,
+		width = 35,
+		height = 35,
+		frameOn = 1,
+		frameOff = 2,
+		x = 63,
+		y = 125,
+		style = "checkbox",
+		id = "useIncentivizedRewarded",
+		initialSwitchState = false,
+		onPress = uiEvent
+	})
+mainGroup:insert( irSwitch )
+local irLabel = display.newText( mainGroup, "use incentivized/rewarded", irSwitch.x+22, irSwitch.y, appFont, 16 )
+irLabel.anchorX = 0
 
 -- Create buttons
 loadButton = widget.newButton(
@@ -152,7 +214,7 @@ loadButton = widget.newButton(
 		fontSize = 16,
 		fillColor = { default={ 0.16,0.36,0.56,1 }, over={ 0.16,0.36,0.56,1 } },
 		labelColor = { default={ 1,1,1,1 }, over={ 1,1,1,0.8 } },
-		onRelease = onButtonRelease
+		onRelease = uiEvent
 	})
 loadButton:setEnabled( false )
 loadButton.alpha = 0.3
@@ -171,7 +233,7 @@ showButton = widget.newButton(
 		fontSize = 16,
 		fillColor = { default={ 0.16,0.36,0.56,1 }, over={ 0.16,0.36,0.56,1 } },
 		labelColor = { default={ 1,1,1,1 }, over={ 1,1,1,0.8 } },
-		onRelease = onButtonRelease
+		onRelease = uiEvent
 	})
 showButton:setEnabled( false )
 showButton.alpha = 0.3
